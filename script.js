7a34abfd396d9644ffa1750abf395b384e63afb5
JacobMu <31470133+JacobMu@users.noreply.github.com>
2017-10-04 00:23:04 +0200

    Add files via upload
    changed JS file

--- script.js
+++ script.js
@@ -2,17 +2,15 @@ var today;
 
 today = new Date();
   var day = today.getDay();
-  var daylist = ["Sunday","Monday","Tuesday","Wednesday ","Thursday","Friday","Saturday"];
+  var daylist = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 
 function isWednesday() {
    if (daylist[day] === "Wednesday") {
       console.log("It's Wednesday, my dudes.");
-
    }  else {
       console.log("It's not Wednesday.");
       document.querySelector('.dude').style.display='none';
       document.getElementById('notWednesday').style.display='block';
    }
 }
-
 isWednesday();
